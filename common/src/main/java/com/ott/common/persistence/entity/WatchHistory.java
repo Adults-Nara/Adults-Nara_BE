1756c0640158f90199874bba4d44314997ffc675
meisseoyeon <dkssud1191@naver.com>
2026-02-11 15:38:12 +0900

    [ASN-31] fix: baseEntity 수정 및 유니크설정 추가

--- common/src/main/java/com/ott/common/persistence/entity/WatchHistory.java
+++ common/src/main/java/com/ott/common/persistence/entity/WatchHistory.java
@@ -1,15 +1,15 @@
 package com.ott.common.persistence.entity;
 
+import com.ott.common.persistence.base.BaseEntity;
 import com.ott.common.util.IdGenerator;
 import jakarta.persistence.*;
 import lombok.Getter;
 
-import java.time.OffsetDateTime;
 
 @Entity
 @Getter
 @Table(name = "watch_history")
-public class WatchHistory {
+public class WatchHistory extends BaseEntity {
 
     @Id
     @Column(name = "watch_history_id")
@@ -29,15 +29,6 @@ public class WatchHistory {
     @Column(nullable = false)
     private boolean completed;
 
-    @Column(nullable = false)
-    private boolean disliked;
-
-    @Column(name = "created_at")
-    private OffsetDateTime createdAt;
-
-    @Column(name = "updated_at")
-    private OffsetDateTime updatedAt;
-
     protected WatchHistory() {}
 
     public WatchHistory(User user, VideoMetadata videoMetadata) {
@@ -45,30 +36,18 @@ public class WatchHistory {
         this.videoMetadata = videoMetadata;
         this.lastPosition = 0;
         this.completed = false;
-        this.disliked = false;
-        this.createdAt = OffsetDateTime.now();
-        this.updatedAt = OffsetDateTime.now();
     }
 
     @PrePersist
     private void prePersist() {
         if (id == null) id = IdGenerator.generate();
-        if (createdAt == null) createdAt = OffsetDateTime.now();
-        if (updatedAt == null) updatedAt = OffsetDateTime.now();
     }
 
     public void updatePosition(int seconds) {
         this.lastPosition = seconds;
-        this.updatedAt = OffsetDateTime.now();
     }
 
     public void markCompleted() {
         this.completed = true;
-        this.updatedAt = OffsetDateTime.now();
-    }
-
-    public void setDisliked(boolean disliked) {
-        this.disliked = disliked;
-        this.updatedAt = OffsetDateTime.now();
     }
 }
