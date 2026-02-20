a269bdf8b349a2d9b4d3fb83ea86b700b91f73f2
meisseoyeon <dkssud1191@naver.com>
2026-02-20 17:21:07 +0900

    [ASN-49] feat: Spring ApplicationEvent 로직 추가 추가

--- apps/core-api/src/main/java/com/ott/core/modules/interaction/service/InteractionService.java
+++ apps/core-api/src/main/java/com/ott/core/modules/interaction/service/InteractionService.java
@@ -5,9 +5,11 @@ import com.ott.common.error.ErrorCode;
 import com.ott.common.persistence.entity.*;
 import com.ott.common.persistence.enums.InteractionType;
 import com.ott.core.modules.interaction.repository.InteractionRepository;
+import com.ott.core.modules.preference.event.InteractionEvent;
 import com.ott.core.modules.user.repository.UserRepository;
 import com.ott.core.modules.video.repository.VideoMetadataRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,7 @@ public class InteractionService {
     private final InteractionRepository interactionRepository;
     private final UserRepository userRepository;
     private final VideoMetadataRepository videoMetadataRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     public void interact(Long userId, Long videoId, InteractionType newType) {
         User user = findUser(userId);
@@ -36,6 +39,9 @@ public class InteractionService {
                     // 같은 버튼 또 누름 -> 취소
                     interactionRepository.delete(interaction);
                     decreaseCount(video.getId(), oldType);
+
+                    // [이벤트 발행] 취소되었으므로 newType은 null로 보냄
+                    eventPublisher.publishEvent(new InteractionEvent(userId, videoId, oldType, null));
                 } else {
                     // 변경
                     interaction.changeType(newType);
@@ -43,15 +49,18 @@ public class InteractionService {
 
                     decreaseCount(video.getId(), oldType); // 기존 거 깎고
                     increaseCount(video.getId(), newType); // 새 거 올림
+                    // [이벤트 발행] 변경 전/후 타입 모두 보냄
+                    eventPublisher.publishEvent(new InteractionEvent(userId, videoId, oldType, newType));
                 }
             } else {
                 // 생성
                 Interaction newInteraction = new Interaction(user, video, newType);
                 interactionRepository.save(newInteraction);
                 increaseCount(video.getId(), newType);
+                // [이벤트 발행] 새로 생성되었으므로 oldType은 null로 보냄
+                eventPublisher.publishEvent(new InteractionEvent(userId, videoId, null, newType));
             }
         }
-
 
     // 조회
     @Transactional(readOnly = true)
