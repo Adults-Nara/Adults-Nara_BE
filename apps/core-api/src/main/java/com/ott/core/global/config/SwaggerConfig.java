5ecb39ffdfe871f5ddb598ddfffca1a97773d7fe
Ashmin <dbswoals0512@gmail.com>
2026-02-24 14:06:59 +0900

    fix: Swagger API Auth 토큰 입력 칸 추가

--- apps/core-api/src/main/java/com/ott/core/global/config/SwaggerConfig.java
+++ apps/core-api/src/main/java/com/ott/core/global/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.ott.core.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,8 @@ import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     @Profile("prod")
     public OpenAPI openApiProd() {
@@ -22,5 +27,21 @@ public class SwaggerConfig {
     public OpenAPI openApiLocal() {
         return new OpenAPI()
                 .servers(List.of(new Server().url("http://localhost:8080")));
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                // 1) Security Scheme 정의
+                .components(new Components().addSecuritySchemes(
+                        SECURITY_SCHEME_NAME,
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                // 2) 전역 Security Requirement (모든 API에 기본 적용)
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
     }
 }
