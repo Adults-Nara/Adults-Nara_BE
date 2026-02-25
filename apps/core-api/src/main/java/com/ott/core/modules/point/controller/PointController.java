ad43d45dcb072b85e2521cedb1d0ff6e81604628
cylin0201 <cylin0201@naver.com>
2026-02-25 16:03:07 +0900

    [ASN-79] fix: userId를 받아오도록 수정

--- apps/core-api/src/main/java/com/ott/core/modules/point/controller/PointController.java
+++ apps/core-api/src/main/java/com/ott/core/modules/point/controller/PointController.java
@@ -3,12 +3,12 @@ package com.ott.core.modules.point.controller;
 import com.ott.common.response.ApiResponse;
 import com.ott.core.modules.point.dto.PointTransactionHistoryRequest;
 import com.ott.core.modules.point.dto.PointTransactionHistoryResponse;
+import com.ott.core.modules.point.dto.ProductPurchaseRequest;
 import com.ott.core.modules.point.dto.UserPointBalanceResponse;
 import com.ott.core.modules.point.service.PointService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,13 +19,26 @@ public class PointController {
     private final PointService pointService;
 
     @GetMapping
-    public ApiResponse<UserPointBalanceResponse> getMyPointBalance(Long userId) {
+    public ApiResponse<UserPointBalanceResponse> getMyPointBalance(
+            @AuthenticationPrincipal Long userId) {
         return ApiResponse.success(pointService.findUserCurrentPoint(userId));
     }
 
     @GetMapping("/details")
-    public ApiResponse<List<PointTransactionHistoryResponse>> getMyPointTransactionHistory(Long userId, PointTransactionHistoryRequest req) {
+    public ApiResponse<List<PointTransactionHistoryResponse>> getMyPointTransactionHistory(
+            @AuthenticationPrincipal Long userId,
+            PointTransactionHistoryRequest req) {
         return ApiResponse.success(pointService.findUserPointHistory(userId, req));
     }
 
+    @PostMapping("/purchase/reward")
+    public ApiResponse<Void> rewardPurchasePoint(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody ProductPurchaseRequest request) {
+
+        // 내부적으로 DuplicateKeyException 발생 시 멱등성을 지키기 위해 BusinessException으로 차단됨
+        pointService.rewardPurchaseReward(userId, request);
+
+        return ApiResponse.success(null);
+    }
 }
