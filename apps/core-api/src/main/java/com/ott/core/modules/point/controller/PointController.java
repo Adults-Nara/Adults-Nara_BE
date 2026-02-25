0ecb9e3780baa6e5b1550143e6f6499670881f89
cylin0201 <cylin0201@naver.com>
2026-02-25 15:39:54 +0900

    [ASN-79] fix: import 문 삭제

--- apps/core-api/src/main/java/com/ott/core/modules/point/controller/PointController.java
+++ apps/core-api/src/main/java/com/ott/core/modules/point/controller/PointController.java
@@ -1,7 +1,6 @@
 package com.ott.core.modules.point.controller;
 
 import com.ott.common.response.ApiResponse;
-import com.ott.core.modules.point.dto.PointTransactionHistoryDTO;
 import com.ott.core.modules.point.dto.PointTransactionHistoryRequest;
 import com.ott.core.modules.point.dto.PointTransactionHistoryResponse;
 import com.ott.core.modules.point.dto.UserPointBalanceResponse;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 
 @RestController
