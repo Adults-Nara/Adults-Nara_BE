39b118c3b1917b3bda3d3eacb089952613774616
HoonSeokChoi <gnstjrdlsla18@gmail.com>
2026-02-26 17:01:20 +0900

    [ASN-71] 찜한 영상 재생목록 카드 및 영상 리스트 조회 기능 구현

--- apps/core-api/src/main/java/com/ott/core/modules/bookmark/repository/BookmarkRepository.java
+++ apps/core-api/src/main/java/com/ott/core/modules/bookmark/repository/BookmarkRepository.java
@@ -3,10 +3,14 @@ package com.ott.core.modules.bookmark.repository;
 import com.ott.common.persistence.entity.Bookmark;
 import com.ott.common.persistence.entity.User;
 import com.ott.common.persistence.entity.VideoMetadata;
+import com.ott.common.persistence.enums.VideoType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
@@ -20,4 +24,33 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Optional<Bookmark> findByUserIdAndVideoId(@Param("userId") Long userId, @Param("videoId") Long videoId);
 
     boolean existsByUserIdAndVideoMetadata_VideoId(Long userId, Long videoId);
+
+    @Query("""
+             SELECT vm.thumbnailUrl FROM Bookmark b
+             JOIN b.videoMetadata vm
+             WHERE b.user.id = :userId
+                 AND vm.videoType = :videoType
+                 AND vm.deleted = false
+             ORDER BY b.createdAt DESC
+            """)
+    List<String> findThumbnailByUserIdAndVideoType(@Param("userId") Long userId, @Param("videoType") VideoType videoType, Pageable pageable);
+
+    @Query("""
+             SELECT COUNT(b) FROM Bookmark b
+             JOIN b.videoMetadata vm
+             WHERE b.user.id = :userId
+                AND vm.videoType = :videoType
+                AND vm.deleted = false
+            """)
+    long countByUserIdAndVideoType(@Param("userId") Long userId, @Param("videoType") VideoType videoType);
+
+    @Query("""
+            SELECT b FROM Bookmark b
+            JOIN FETCH b.videoMetadata vm
+            WHERE b.user.id = :userId
+                AND vm.videoType = :videoType
+                AND vm.deleted = false
+            ORDER BY b.createdAt DESC
+            """)
+    Slice<Bookmark> findByUserIdAndVideoType(@Param("userId") Long userId, @Param("videoType") VideoType videoType, Pageable pageable);
 }
