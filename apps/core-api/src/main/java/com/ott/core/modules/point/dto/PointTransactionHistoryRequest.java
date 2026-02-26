f060fe138de6f8ae26435eb15cc38422c94b12f4
cylin0201 <cylin0201@naver.com>
2026-02-26 15:27:39 +0900

    [ASN-85] feat: Point 도메인 컨트롤러에 협업을 위한 swagger 어노테이션 추가

--- apps/core-api/src/main/java/com/ott/core/modules/point/dto/PointTransactionHistoryRequest.java
+++ apps/core-api/src/main/java/com/ott/core/modules/point/dto/PointTransactionHistoryRequest.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PointTransactionHistoryRequest {
+    @Schema(description = "조회 시작일", example = "2026-02-01", type = "string")
     private String startDate; // "yyyy-MM-dd" 형식
+    @Schema(description = "조회 종료일", example = "2026-02-25", type = "string")
     private String endDate;
 }
